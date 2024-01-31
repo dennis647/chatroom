cd95ee1f6505719b6bb2a6cb38da943b42da112a
dennis647 <dennis.brokking@gmail.com>
2024-01-31 10:27:32 +0100

    Working connection w/ firebase

--- main/main.js
+++ main/main.js
@@ -1,9 +1,38 @@
-var database = firebase.database();
+// main.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js';
+import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js';
 
-database.ref('data').set({
+const firebaseConfig = {
+  apiKey: "AIzaSyBQZGdqHihNGbuhmwzO-rL7SH4qoElo1Vc",
+  authDomain: "chatroom-8242c.firebaseapp.com",
+  databaseURL: "https://chatroom-8242c-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "chatroom-8242c",
+  storageBucket: "chatroom-8242c.appspot.com",
+  messagingSenderId: "256590892317",
+  appId: "1:256590892317:web:4917a7f2e6117cb09b65f4",
+  measurementId: "G-LS8QHLL32M"
+};
+firebase.initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+
+const dataRef = ref(database, 'data');
+set(dataRef, {
   name: 'Ola Normann',
   age: 19
 });
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log("User is signed in:", user.uid);
+  } else {
+    console.log("No user is signed in.");
+  }
+});
+
+// Your main.js code goes here
+
 
 /*
 // Get a reference to the authentication service and database
