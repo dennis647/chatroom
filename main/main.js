de02b47adcbae90db513d324b614d71a507dcd91
dennis647 <dennis.brokking@gmail.com>
2024-01-31 17:57:01 +0100

    Added user register & started on message sending

--- main/main.js
+++ main/main.js
@@ -1,6 +1,11 @@
+const enterUsername = document.getElementById("loginUsername");
+const enterPassword = document.getElementById("loginPassword");
+const sendMsgBtn = document.getElementById("messageInputBtn");
+const messageText = document.getElementById("messageInputTxt");
+
 // main.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js';
-import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js';
+import { getDatabase, ref, set, remove, onChildAdded, onChildRemoved } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBQZGdqHihNGbuhmwzO-rL7SH4qoElo1Vc",
@@ -13,15 +18,17 @@ const firebaseConfig = {
   measurementId: "G-LS8QHLL32M"
 };
 firebase.initializeApp(firebaseConfig);
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-
+/*
 const dataRef = ref(database, 'data');
 set(dataRef, {
-  name: 'Ola Normann',
-  age: 19
+  username: 'dennis647',
+  password: '1234'
 });
+*/
+
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
@@ -31,61 +38,12 @@ firebase.auth().onAuthStateChanged((user) => {
   }
 });
 
-// Your main.js code goes here
+const sendMsgFunc = () => {
+  var message = messageText.value;
+  var timestamp = new Date().getTime();
+  set(ref(database,"messages/"+timestamp), {
+    message : message
+  }
+)};
 
-
-/*
-// Get a reference to the authentication service and database
-
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      console.log("User is signed in:", user.uid);
-    } else {
-      console.log("No user is signed in.");
-    }
-  });
-
-
-
-  // Sign up
-  firebase.auth().createUser(userName, password)
-    .then((userCredential) => {
-      // User registered successfully
-      var user = userCredential.user;
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.error(errorMessage);
-    });
-
-  // Sign in
-  firebase.auth().userSignIn(userName, password)
-    .then((userCredential) => {
-      // User signed in successfully
-      var user = userCredential.user;
-    })
-
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.error(errorMessage);
-      alert('This user doesnt exist');
-    });
-
-  // Sign out
-  firebase.auth().signOut().then(() => {
-    // User signed out successfully
-  }).catch((error) => {
-    console.error(error.message);
-  });
-
-
-  const db = firebase.database();
-
-  db.ref("messages").push({
-    sender: userId,
-    message: "Hello, Firebase!",
-    timestamp: firebase.database.ServerValue.TIMESTAMP,
-  });
-*/
+sendMsgBtn.addEventListener("click", sendMsgFunc);
---
+++ register/register.js
@@ -0,0 +1,86 @@
+const registerUsername = document.getElementById("registerUsern");
+const registerPassword = document.getElementById("registerPassword");
+const registerBtn = document.getElementById("registerBtn");
+const registerEmail = document.getElementById("registerEmail");
+
+
+// Add in firebase
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js';
+import { getDatabase, ref, set, remove, onChildAdded, onChildRemoved } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQZGdqHihNGbuhmwzO-rL7SH4qoElo1Vc",
+  authDomain: "chatroom-8242c.firebaseapp.com",
+  databaseURL: "https://chatroom-8242c-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "chatroom-8242c",
+  storageBucket: "chatroom-8242c.appspot.com",
+  messagingSenderId: "256590892317",
+  appId: "1:256590892317:web:4917a7f2e6117cb09b65f4",
+  measurementId: "G-LS8QHLL32M"
+};
+
+firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = firebase.auth();
+const db = firebase.database();
+
+// Registering as a new user
+const registerUser = () => {
+ var newUsername = registerUsername.value;
+ var newUserPassword = registerPassword.value;
+ var email = registerEmail.value;
+
+ if (validate_email(email) == false){
+  alert("This isn't a valid email adress");
+  return
+ }
+
+  if (validateField(newUsername) == false || validateField(newUserPassword) == false) {
+    alert('You need to input username & password');
+    return;
+  }
+
+  // Register the user in firebase
+  auth.createUserWithEmailAndPassword(email, newUserPassword)
+  .then(function() {
+
+    var user = auth.currentUser;
+
+    var dbRef = db.ref();
+    var userData = {
+      email : email,
+      newUsername : newUsername,
+      lastLogin : Date.now()
+    }
+
+    dbRef.child("users/"+user.uid).set(userData);
+
+    alert(`User has been created`);
+
+  })
+  .catch(function(error) {
+    var error_msg = error.message;
+
+    alert(error_msg)
+  })
+  }
+
+const validate_email = (email) => {
+  var expression = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  if (expression.test(email) == true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+ const validateField = (field) => {
+  if (field == null || field <= 0 ) {
+    return false;
+  } else {
+    return true;
+  }
+ }
+
+ registerBtn.addEventListener("click", registerUser);
